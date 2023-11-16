34cae13bdfe2d8612351d8b7eb7db8f8cc4fdf93
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-11-16 17:50:41 +0700

    feat(.env.example): add .env.example file to provide a template for environment variables chore(.gitignore): add .env file to .gitignore to prevent it from being tracked chore(app/layout.tsx): update layout component to adjust the layout structure and styling chore(app/login/layout.tsx): create a new LoginLayout component for the login page chore(app/login/page.tsx): create a new LoginPage component for the login page chore(components/navbar.tsx): update Navbar component to include login link in the navigation menu chore(config/site.ts): update siteConfig to include login link in the navigation menu feat(libs/db.ts): create a new db module to handle database connection using PrismaClient chore(package.json): add @prisma/client and prisma as devDependencies feat(prisma/schema.prisma): create Prisma schema file to define the database schema

---
+++ app/login/page.tsx
@@ -0,0 +1,66 @@
+import { title } from "@/components/primitives";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  Input,
+  CardFooter,
+  Button,
+  Divider,
+  Checkbox,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { BsGithub } from "react-icons/bs";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+export default function LoginPage() {
+  return (
+    <Card className="max-w-md px-2 py-4 w-96" shadow="lg">
+      <CardHeader className="flex-col items-center justify-center px-4 pt-2 pb-5">
+        <h4 className="font-bold text-center text-large">Login Form</h4>
+      </CardHeader>
+      <Divider />
+      <form>
+        <CardBody className="py-5 overflow-visible gap-y-5">
+          <Input type="email" label="Email" isRequired size="sm" />
+          <Input type="password" label="Password" isRequired size="sm" />
+          <Checkbox isRequired size="sm">
+            Remember Me
+          </Checkbox>
+        </CardBody>
+        <CardFooter>
+          <div className="flex flex-col w-full gap-y-3">
+            <div className="flex gap-x-3">
+              <Button
+                color="default"
+                variant="bordered"
+                startContent={<FcGoogle size={20} />}
+              >
+                Google
+              </Button>
+              <Button
+                color="default"
+                variant="bordered"
+                startContent={<BsGithub size={20} />}
+              >
+                Github
+              </Button>
+              <Button
+                color="default"
+                variant="bordered"
+                startContent={<FaSquareXTwitter size={20} />}
+              >
+                Twitter
+              </Button>
+            </div>
+            <Button type="submit" color="primary" className="w-full mt-3">
+              Submit
+            </Button>
+          </div>
+        </CardFooter>
+      </form>
+    </Card>
+  );
+}
